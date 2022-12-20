47b7eb54cdc567af64439952c954db296ebee9d9
hnam <hnam4527@gmail.com>
2022-12-20 12:46:12 +0700

    update download cv

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import './banner.scss'
+import React from "react";
+// @ts-ignore
+import CV from "../../asset/HNNCV.pdf";
+import "./banner.scss";
 const Banner = () => {
-    return (
-        <div className='banner'>
-            <div className="banner-container">
-                <h2 className="">Hello,</h2>
-                <h2 className="">I'm Huynh Nhut Nam</h2>
-                <h3 className="">I'm frontend developer</h3>
-            </div>
-            <div className="banner-resume">
-                <div className="">
-                    My Resume
+  return (
+    <div className="banner">
+      <div className="banner-container">
+        <h2 className="">Hello,</h2>
+        <h2 className="">I'm Huynh Nhut Nam</h2>
+        <h3 className="">I'm frontend developer</h3>
+      </div>
+      <div className="banner-resume">
+        <a href={CV} download className="">
+          My Resume
+        </a>
+      </div>
+    </div>
+  );
+};
 
-                </div>
-            </div>
-        </div>
-    )
-}
-
-export default Banner
+export default Banner;
